82082c5896fb4b6e362e43880e2666956aac682f
Nebiyu Musbah <nebiyumusbah378@gmail.com>
2025-06-27 14:57:01 +0300

    fix env and dockerignore

---
+++ Backend/internal/domain/interfaces/jwt.go
@@ -0,0 +1,8 @@
+package interfaces
+
+import "addis-hiwot/internal/domain/models"
+
+type JWTService interface {
+	GenerateToken(user *models.User) (string, error)
+	// ValidateToken(token string) (*models.UserClaims, error)
+}
