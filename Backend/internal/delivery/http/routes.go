2f07d5f07fe04df52a7ac6f8da216acd6486680c
dagota12 <chernetdagim@gmail.com>
2025-06-27 22:40:50 +0300

    feat: implement auth integration with jwt and session managment

--- Backend/internal/delivery/http/routes.go
+++ Backend/internal/delivery/http/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"addis-hiwot/internal/config"
 	"addis-hiwot/internal/delivery/http/handlers"
+	"addis-hiwot/internal/delivery/middlewares"
 	"addis-hiwot/internal/repository"
 	"addis-hiwot/internal/service"
 	"addis-hiwot/internal/usecases"
@@ -20,6 +21,8 @@ func SetupRoutes(r *gin.Engine, cfg *config.Config) {
 	userUC := usecases.NewUserUsecase(userRepo, jwtService)
 	userHandler := handlers.NewUserHandler(userUC)
 
+	sessionRepo := repository.NewSessionRepository(db)
+	middleware := middlewares.New(sessionRepo)
 	api := r.Group("/api/v1")
 
 	// AUthentication routes
@@ -32,4 +35,17 @@ func SetupRoutes(r *gin.Engine, cfg *config.Config) {
 	{
 		api.GET("/users", userHandler.GetUsers)
 	}
+
+	auth := api.Group("/auth")
+	authRepo := repository.NewAuthRepository(db)
+	authUC := usecases.NewAuthUsecase(authRepo, sessionRepo)
+	authHandler := handlers.NewAuthHander(authUC)
+	{
+		auth.POST("/register", authHandler.Register)
+		auth.POST("/login", authHandler.Login)
+		auth.POST("/logout", middleware.AuthMiddleware(), authHandler.Logout)
+	}
+	api.GET("/protected", middleware.AuthMiddleware(), middlewares.CheckRoles("user"), func(ctx *gin.Context) {
+		ctx.JSON(200, "success")
+	})
 }
--- Backend/internal/delivery/middlewares/auth_middleware.go
+++ Backend/internal/delivery/middlewares/auth_middleware.go
@@ -1,1 +1,77 @@
 package middlewares
+
+import (
+	"addis-hiwot/internal/domain/schema"
+	"addis-hiwot/internal/repository"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Middleware struct {
+	sr repository.SessionRepository
+}
+
+func New(sr repository.SessionRepository) *Middleware {
+	return &Middleware{sr}
+}
+func (m *Middleware) AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if !strings.HasPrefix(authHeader, "Bearer ") {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "missing token"})
+			return
+		}
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+		claims := &schema.AuthClaim{}
+
+		token, err := jwt.ParseWithClaims(tokenStr, claims, func(t *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("JWT_AUTH_SECRET")), nil
+		})
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			return
+		}
+		if !token.Valid {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+			return
+		}
+		//if user logged out before the token expired
+		_, err = m.sr.Get(tokenStr)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+		}
+		claims, ok := token.Claims.(*schema.AuthClaim)
+		if !ok {
+			log.Printf("[middleware] type mismatch claim is not *schema.AuthClaim  got '%T'", token.Claims)
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "unknown claims type, cannot proceed"})
+			return
+		}
+		c.Set("claim", claims)
+		c.Next()
+	}
+}
+
+func CheckRoles(roles ...string) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		claim, ok := ctx.MustGet("claim").(*schema.AuthClaim)
+		if !ok {
+			log.Printf("[middleware] type mismatch claim is not *schema.AuthClaim  got '%T'", ctx.MustGet("claim"))
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "unknown claims type, cannot proceed"})
+			return
+		}
+		for _, role := range roles {
+			if claim.Role == role {
+				ctx.Next()
+				return
+			}
+		}
+		ctx.JSON(http.StatusForbidden, gin.H{"error": "Forbidden: You don't have permission to access this resource"})
+		ctx.Abort()
+	}
+
+}
