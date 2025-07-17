8263f1fa3098dd0040435c2c806c5b7f17f61873
Kalki767 <kalkidankidane3@gmail.com>
2025-07-17 09:29:50 +0300

    Implement get all messages logic and applied pagination for faster load time

--- Backend/internal/repository/community_repo.go
+++ Backend/internal/repository/community_repo.go
@@ -2,6 +2,8 @@ package repository
 
 import (
 	"addis-hiwot/internal/domain/models"
+	"context"
+	"time"
 
 	"gorm.io/gorm"
 )
@@ -13,13 +15,30 @@ type communityRepository struct {
 func NewCommunityRepository(db *gorm.DB) *communityRepository {
 	return &communityRepository{db: db}
 }
-func (r *communityRepository) GetAllMessages() ([]*models.CommunityMessage, error) {
+// GetAllMessages retrieves up to 'limit' community messages posted before the specified time.
+// It returns the messages, a boolean indicating if there are more messages, the timestamp for the next page, and an error if any.
+// Pagination is handled by fetching 'limit + 1' messages and trimming the result if more exist.
+func (r *communityRepository) GetAllMessages(ctx context.Context, limit int, before time.Time) ([]*models.CommunityMessage, bool, time.Time, error) {
 	var messages []*models.CommunityMessage
-	err := r.db.Preload("User").Order("posted_at DESC").Find(&messages).Error
+
+	query := limit + 1
+
+	err := r.db.WithContext(ctx).Where("PostedAt < ?", before).Order("PostedAt DESC").Limit(query).Preload("User").Find(&messages).Error
 	if err != nil {
-		return nil, err
+		return nil, false, time.Time{}, err
 	}
-	return messages, nil
+	if len(messages) == 0 {
+		return nil, false, time.Time{}, nil
+	}
+	hasMore := len(messages) > limit
+	if hasMore {
+		messages = messages[:limit]
+	}
+	var nextPageTime time.Time
+	if len(messages) > 0 {
+		nextPageTime = messages[len(messages)-1].PostedAt
+	}
+	return messages, hasMore, nextPageTime, nil
 }
 
 func (r *communityRepository) CreateMessage(message *models.CommunityMessage) (*models.CommunityMessage, error) {
